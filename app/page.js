5ef8f330956e52047160da51ea6dff3fbfc4e79b
TokenRevoke <tokenrevoke@proton.me>
2023-08-30 13:23:15 -0700

    first commit

---
+++ app/page.js
@@ -0,0 +1,17 @@
+
+export default function Home(){
+  const backgroundstyle = {
+    background:`url("/banner.jpg")`,
+    backgroundSize:"100%",
+    width:"100%",
+    height:"100vh",
+    backgroundRepeat:"no-repeat",
+    backgroundPosition:"center",
+
+  }
+  return (
+    <div style={backgroundstyle}>
+
+    </div>
+  )
+}
